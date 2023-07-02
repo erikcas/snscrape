87db06f7adbe97e6644e2ddb06d64226ccf5a49e
Erik Castricum <git@cas-online.nl>
2023-07-02 17:21:23 +0200

    [WORKAROUND][TwitterSearchScrape] user agent
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- snscrape/modules/twitter.py
+++ snscrape/modules/twitter.py
@@ -99,7 +99,7 @@ _API_AUTHORIZATION_HEADER = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOu
 _globalGuestTokenManager = None
 _GUEST_TOKEN_VALIDITY = 10800
 _CIPHERS_CHROME = 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA:AES256-SHA'
-
+_USER_AGENT = 'Googlebot'
 
 @dataclasses.dataclass
 class Tweet(snscrape.base.Item):
@@ -806,6 +806,7 @@ class _TwitterAPIScraper(snscrape.base.Scraper):
 		self._guestTokenManager = guestTokenManager
 		self._maxEmptyPages = maxEmptyPages
 		self._apiHeaders = {
+            'User-Agent': _USER_AGENT,
 			'Authorization': _API_AUTHORIZATION_HEADER,
 			'Referer': self._baseUrl,
 			'Accept-Language': 'en-US,en;q=0.5',
